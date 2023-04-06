8f546865599d91e6fd545e3b882c4be47f4ab8fd
Hung Pham <pham.quang.thanh.hung@gmail.com>
2023-04-06 16:43:47 +0300

    Improve codes by using count object

--- main.tf
+++ main.tf
@@ -7,39 +7,25 @@ resource "aws_vpc" "main" {
   }
 }
 
-resource "aws_subnet" "public0" {
+resource "aws_subnet" "public" {
+  count = length(var.public_cidr)
+
   vpc_id     = aws_vpc.main.id
-  cidr_block = var.public_cidr[0]
+  cidr_block = var.public_cidr[count.index]
 
   tags = {
-    Name = "${var.area_code}-public0"
+    Name = "${var.area_code}-public${count.index}"
   }
 }
 
-resource "aws_subnet" "public1" {
+resource "aws_subnet" "private" {
+  count = length(var.private_cidr)
+
   vpc_id     = aws_vpc.main.id
-  cidr_block = var.public_cidr[1]
+  cidr_block = var.private_cidr[count.index]
 
   tags = {
-    Name = "${var.area_code}-public1"
-  }
-}
-
-resource "aws_subnet" "private0" {
-  vpc_id     = aws_vpc.main.id
-  cidr_block = var.private_cidr[0]
-
-  tags = {
-    Name = "${var.area_code}-private0"
-  }
-}
-
-resource "aws_subnet" "private1" {
-  vpc_id     = aws_vpc.main.id
-  cidr_block = var.private_cidr[1]
-
-  tags = {
-    Name = "${var.area_code}-private1"
+    Name = "${var.area_code}-private${count.index}"
   }
 }
 
@@ -52,39 +38,23 @@ resource "aws_internet_gateway" "main" {
 }
 
 # create Elastic Ip address for NAT
-resource "aws_eip" "nat0" {
+resource "aws_eip" "nat" {
+  count = 2
+
   vpc = true
 
   tags = {
-    Name = "${var.area_code}-nat0"
+    Name = "${var.area_code}-nat${count.index}"
   }
 }
 
-# create Elastic Ipaddress for NAT
-resource "aws_eip" "nat1" {
-  vpc = true
+resource "aws_nat_gateway" "public" {
+  count = 2
+  allocation_id = aws_eip.nat[count.index].id
+  subnet_id     = aws_subnet.public[count.index].id
 
   tags = {
-    Name = "${var.area_code}-nat1"
-  }
-}
-
-resource "aws_nat_gateway" "public0" {
-  allocation_id = aws_eip.nat0.id
-  subnet_id     = aws_subnet.public0.id
-
-  tags = {
-    Name = "${var.area_code}-public0"
-  }
-
-}
-
-resource "aws_nat_gateway" "public1" {
-  allocation_id = aws_eip.nat1.id
-  subnet_id     = aws_subnet.public1.id
-
-  tags = {
-    Name = "${var.area_code}-public1"
+    Name = "${var.area_code}-public${count.index}"
   }
 
 }
@@ -102,52 +72,34 @@ resource "aws_route_table" "public" {
   }
 }
 
-resource "aws_route_table" "private0" {
+resource "aws_route_table" "private" {
+  count = 2
+
   vpc_id = aws_vpc.main.id
 
   route {
     # why we use 0.0.0.0/0, not 10.0.3.0? 
     # Does it mean all the host in private subnet can go to either public0 or public1 if one of those are down?
     cidr_block     = "0.0.0.0/0"
-    nat_gateway_id = aws_nat_gateway.public0.id
+    nat_gateway_id = aws_nat_gateway.public[count.index].id
   }
 
   tags = {
-    Name = "${var.area_code}-private0"
+    Name = "${var.area_code}-private${count.index}"
   }
 }
 
-resource "aws_route_table" "private1" {
-  vpc_id = aws_vpc.main.id
 
-  route {
-    # why we use 0.0.0.0/0, not 10.0.3.0? 
-    # Does it mean all the host in private subnet can go to either public0 or public1 if one of those are down?
-    cidr_block     = "0.0.0.0/0"
-    nat_gateway_id = aws_nat_gateway.public1.id
+resource "aws_route_table_association" "public" {
+  count = 2
 
-  }
-  tags = {
-    Name = "${var.area_code}-private1"
-  }
-}
-
-resource "aws_route_table_association" "public0" {
-  subnet_id      = aws_subnet.public0.id
+  subnet_id      = aws_subnet.public[count.index].id
   route_table_id = aws_route_table.public.id
 }
 
-resource "aws_route_table_association" "public1" {
-  subnet_id      = aws_subnet.public1.id
-  route_table_id = aws_route_table.public.id
-}
+resource "aws_route_table_association" "private" {
+  count = 2
 
-resource "aws_route_table_association" "private0" {
-  subnet_id      = aws_subnet.private0.id
-  route_table_id = aws_route_table.private0.id
-}
-
-resource "aws_route_table_association" "private1" {
-  subnet_id      = aws_subnet.private1.id
-  route_table_id = aws_route_table.private1.id
+  subnet_id      = aws_subnet.private[count.index].id
+  route_table_id = aws_route_table.private[count.index].id
 }
