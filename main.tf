0342d6c58974e6c4b61653d0cb72334f9943c3ce
Hung Pham <pham.quang.thanh.hung@gmail.com>
2023-04-06 16:02:44 +0300

    Using variable and setting their values

--- main.tf
+++ main.tf
@@ -1,45 +1,45 @@
 # Create a VPC
 resource "aws_vpc" "main" {
-  cidr_block = "10.0.0.0/16"
+  cidr_block = var.vpc_cidr
 
   tags = {
-    Name = "Main"
+    Name = var.area_code
   }
 }
 
 resource "aws_subnet" "public0" {
   vpc_id     = aws_vpc.main.id
-  cidr_block = "10.0.0.0/24"
+  cidr_block = var.public_cidr[0]
 
   tags = {
-    Name = "public0"
+    Name = "${var.area_code}-public0"
   }
 }
 
 resource "aws_subnet" "public1" {
   vpc_id     = aws_vpc.main.id
-  cidr_block = "10.0.1.0/24"
+  cidr_block = var.public_cidr[1]
 
   tags = {
-    Name = "public1"
+    Name = "${var.area_code}-public1"
   }
 }
 
 resource "aws_subnet" "private0" {
   vpc_id     = aws_vpc.main.id
-  cidr_block = "10.0.2.0/24"
+  cidr_block = var.private_cidr[0]
 
   tags = {
-    Name = "private0"
+    Name = "${var.area_code}-private0"
   }
 }
 
 resource "aws_subnet" "private1" {
   vpc_id     = aws_vpc.main.id
-  cidr_block = "10.0.3.0/24"
+  cidr_block = var.private_cidr[1]
 
   tags = {
-    Name = "private1"
+    Name = "${var.area_code}-private1"
   }
 }
 
@@ -47,7 +47,7 @@ resource "aws_internet_gateway" "main" {
   vpc_id = aws_vpc.main.id
 
   tags = {
-    Name = "main"
+    Name = var.area_code
   }
 }
 
@@ -56,7 +56,7 @@ resource "aws_eip" "nat0" {
   vpc = true
 
   tags = {
-    Name = "nat0"
+    Name = "${var.area_code}-nat0"
   }
 }
 
@@ -65,7 +65,7 @@ resource "aws_eip" "nat1" {
   vpc = true
 
   tags = {
-    Name = "nat1"
+    Name = "${var.area_code}-nat1"
   }
 }
 
@@ -74,7 +74,7 @@ resource "aws_nat_gateway" "public0" {
   subnet_id     = aws_subnet.public0.id
 
   tags = {
-    Name = "public0"
+    Name = "${var.area_code}-public0"
   }
 
 }
@@ -84,12 +84,12 @@ resource "aws_nat_gateway" "public1" {
   subnet_id     = aws_subnet.public1.id
 
   tags = {
-    Name = "public1"
+    Name = "${var.area_code}-public1"
   }
 
 }
 
-resource "aws_route_table" "public_route" {
+resource "aws_route_table" "public" {
   vpc_id = aws_vpc.main.id
 
   route {
@@ -98,11 +98,11 @@ resource "aws_route_table" "public_route" {
   }
 
   tags = {
-    Name = "public_route"
+    Name = "${var.area_code}-public"
   }
 }
 
-resource "aws_route_table" "private_route0" {
+resource "aws_route_table" "private0" {
   vpc_id = aws_vpc.main.id
 
   route {
@@ -113,11 +113,11 @@ resource "aws_route_table" "private_route0" {
   }
 
   tags = {
-    Name = "private_route0"
+    Name = "${var.area_code}-private0"
   }
 }
 
-resource "aws_route_table" "private_route1" {
+resource "aws_route_table" "private1" {
   vpc_id = aws_vpc.main.id
 
   route {
@@ -125,29 +125,29 @@ resource "aws_route_table" "private_route1" {
     # Does it mean all the host in private subnet can go to either public0 or public1 if one of those are down?
     cidr_block     = "0.0.0.0/0"
     nat_gateway_id = aws_nat_gateway.public1.id
-    
+
   }
   tags = {
-    Name = "private_route1"
+    Name = "${var.area_code}-private1"
   }
 }
 
 resource "aws_route_table_association" "public0" {
   subnet_id      = aws_subnet.public0.id
-  route_table_id = aws_route_table.public_route.id
+  route_table_id = aws_route_table.public.id
 }
 
 resource "aws_route_table_association" "public1" {
   subnet_id      = aws_subnet.public1.id
-  route_table_id = aws_route_table.public_route.id
+  route_table_id = aws_route_table.public.id
 }
 
 resource "aws_route_table_association" "private0" {
   subnet_id      = aws_subnet.private0.id
-  route_table_id = aws_route_table.private_route0.id
+  route_table_id = aws_route_table.private0.id
 }
 
 resource "aws_route_table_association" "private1" {
   subnet_id      = aws_subnet.private1.id
-  route_table_id = aws_route_table.private_route1.id
+  route_table_id = aws_route_table.private1.id
 }
