c88d4b12bf04bb6e86d5717b8e3a2108d4f492a6
Hung Pham <pham.quang.thanh.hung@gmail.com>
2023-04-18 19:24:45 +0300

    Create level0, add s3, dynamoDB codes to it

---
+++ level0/instance.tf
@@ -0,0 +1,38 @@
+resource "aws_s3_bucket" "tfstate" {
+  bucket = "terraform-remote-state-40h2b"
+
+  tags = {
+    Name = "tfstate"
+  }
+}
+
+resource "aws_s3_bucket_versioning" "tfstate" {
+  bucket = aws_s3_bucket.tfstate.id
+
+  versioning_configuration {
+    status = "Enabled"
+  }
+}
+
+resource "aws_s3_bucket_server_side_encryption_configuration" "tfstate" {
+  bucket = aws_s3_bucket.tfstate.id
+
+  rule {
+    apply_server_side_encryption_by_default {
+      sse_algorithm = "AES256"
+    }
+  }
+}
+
+resource "aws_dynamodb_table" "terraform-remote-state" {
+  billing_mode   = "PROVISIONED"
+  hash_key       = "LockID"
+  name           = "terraform-remote-state"
+  read_capacity  = 1
+  write_capacity = 1
+  
+  attribute {
+    name = "LockID"
+    type = "S"
+  }
+}
