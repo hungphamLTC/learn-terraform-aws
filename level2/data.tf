eb518b3beaf2657772ce8c9310f5d602cb5aa6f5
Hung Pham <thanhhung.phamquang@gmail.com>
2023-04-19 17:21:38 +0300

    Apply code convention and add trailing lines

--- level2/data.tf
+++ level2/data.tf
@@ -2,8 +2,8 @@ data "terraform_remote_state" "level1" {
   backend = "s3"
 
   config = {
-    bucket         = "terraform-remote-state-40h2b"
-    key            = "level1.tfstate"
-    region         = "us-east-1"
+    bucket = "terraform-remote-state-40h2b"
+    key    = "level1.tfstate"
+    region = "us-east-1"
   }
 }
