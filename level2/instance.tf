43eae6192435124d3e1deff1e085ca040c8218ba
Hung Pham <70536024+hungphamLTC@users.noreply.github.com>
2023-06-27 19:16:31 +0300

    Revert "SSM manager"

--- level2/instance.tf
+++ level2/instance.tf
@@ -19,10 +19,62 @@ data "aws_ami" "ubuntu" {
   }
 }
 
+resource "aws_instance" "public" {
+  ami                         = data.aws_ami.ubuntu.id
+  associate_public_ip_address = true
+  instance_type               = var.instance_type
+  key_name                    = var.key_name
+  vpc_security_group_ids      = [aws_security_group.public.id]
+  subnet_id                   = data.terraform_remote_state.level1.outputs.public_subnet_id[0]
+
+  tags = {
+    Name = "${var.area_code}-public"
+  }
+}
+
+resource "aws_security_group" "public" {
+  name        = "${var.area_code}-public"
+  description = "Security Group"
+  vpc_id      = data.terraform_remote_state.level1.outputs.vpc_id
+
+  ingress {
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = [var.personal_cidr]
+  }
+
+  ingress {
+    description = "HTTP from public"
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = [var.personal_cidr]
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  tags = {
+    Name = "${var.area_code}-public"
+  }
+}
+
 resource "aws_security_group" "private" {
   name        = "${var.area_code}-private"
   description = "Security Group"
   vpc_id      = data.terraform_remote_state.level1.outputs.vpc_id
+
+  ingress {
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = [data.terraform_remote_state.level1.outputs.vpc_cidr]
+  }
 
   ingress {
     description     = "HTTP from load balancer"
