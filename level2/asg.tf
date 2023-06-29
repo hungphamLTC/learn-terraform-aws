3e6b6016580f3035f9dd9d534f3cffbef60f1173
Hung Pham <pham.quang.thanh.hung@gmail.com>
2023-06-29 16:01:55 +0300

    ASG module

--- level2/asg.tf
+++ level2/asg.tf
@@ -1,26 +1,9 @@
-resource "aws_launch_configuration" "main" {
-  name_prefix          = "${var.area_code}-"
-  image_id             = data.aws_ami.ubuntu.id
-  instance_type        = "t2.micro"
-  security_groups      = [aws_security_group.private.id]
-  user_data            = file("install_apache.sh")
-  key_name             = "main11"
-  iam_instance_profile = aws_iam_instance_profile.main.name
-}
+module "asg" {
+  source = "../modules/asg"
 
-resource "aws_autoscaling_group" "main" {
-  name             = var.area_code
-  desired_capacity = 2
-  max_size         = 4
-  min_size         = 2
-
-  target_group_arns    = [aws_lb_target_group.main.arn]
-  launch_configuration = aws_launch_configuration.main.name
-  vpc_zone_identifier  = data.terraform_remote_state.level1.outputs.private_subnet_id
-
-  tag {
-    key                 = "Name"
-    value               = var.area_code
-    propagate_at_launch = true
-  }
+  area_code         = var.area_code
+  vpc_id            = data.terraform_remote_state.level1.output.vpc_id
+  private_subnet_id = data.terraform_remote_state.level1.output.private_subnet_id
+  load_balancer_sg  = aws_security_group.load_balancer.id
+  target_group_arn  = aws_lb_target_group.main.id
 }
--- level2/variables.tf
+++ level2/variables.tf
@@ -1,9 +1,15 @@
-variable "area_code" {
-  type = string
-}
-
 variable "instance_type" {}
+
+variable "area_code" {}
 
 variable "key_name" {}
 
 variable "personal_cidr" {}
+
+variable "private_subnet_id" {}
+
+variable "vpc_id" {}
+
+variable "load_balancer_sg" {}
+
+variable "target_group_arn" {}
