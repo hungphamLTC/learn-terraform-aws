c62b9c16345330bf5f6bcaf6e19b56dd1df3ec80
Hung Pham <pham.quang.thanh.hung@gmail.com>
2023-06-29 17:26:54 +0300

    fixing codes in order to run ASG module

--- level2/asg.tf
+++ level2/asg.tf
@@ -2,8 +2,8 @@ module "asg" {
   source = "../modules/asg"
 
   area_code         = var.area_code
-  vpc_id            = data.terraform_remote_state.level1.output.vpc_id
-  private_subnet_id = data.terraform_remote_state.level1.output.private_subnet_id
+  vpc_id            = data.terraform_remote_state.level1.outputs.vpc_id
+  private_subnet_id = data.terraform_remote_state.level1.outputs.private_subnet_id
   load_balancer_sg  = aws_security_group.load_balancer.id
   target_group_arn  = aws_lb_target_group.main.id
 }
