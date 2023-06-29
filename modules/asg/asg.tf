2bc1cf63159e1200f6c99a1b5358008f20513154
Hung Pham <pham.quang.thanh.hung@gmail.com>
2023-06-29 16:09:01 +0300

    ASG and VPC modules

---
+++ modules/asg/asg.tf
@@ -0,0 +1,46 @@
+data "aws_ami" "ubuntu" {
+  most_recent = true
+  owners      = ["amazon"]
+
+  filter {
+    name   = "name"
+    values = ["ubuntu/images/hvm-ssd/ubuntu-jammy-*-amd64-server-*"]
+  }
+
+  filter {
+    name   = "root-device-type"
+    values = ["ebs"]
+  }
+
+  filter {
+    name   = "virtualization-type"
+    values = ["hvm"]
+  }
+}
+
+resource "aws_launch_configuration" "main" {
+  name_prefix          = "${var.area_code}-"
+  image_id             = data.aws_ami.ubuntu.id
+  instance_type        = "t2.micro"
+  security_groups      = [aws_security_group.private.id]
+  user_data            = file("${path.module}/install_apache.sh")
+  key_name             = "main11"
+  iam_instance_profile = aws_iam_instance_profile.main.name
+}
+
+resource "aws_autoscaling_group" "main" {
+  name             = var.area_code
+  desired_capacity = 2
+  max_size         = 4
+  min_size         = 2
+
+  target_group_arns    = [var.target_group_arn]
+  launch_configuration = aws_launch_configuration.main.name
+  vpc_zone_identifier  = var.private_subnet_id
+
+  tag {
+    key                 = "Name"
+    value               = var.area_code
+    propagate_at_launch = true
+  }
+}
---
+++ modules/asg/variables.tf
@@ -0,0 +1,9 @@
+variable "area_code" {}
+
+variable "private_subnet_id" {}
+
+variable "vpc_id" {}
+
+variable "load_balancer_sg" {}
+
+variable "target_group_arn" {}
