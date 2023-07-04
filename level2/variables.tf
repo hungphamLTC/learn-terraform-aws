7e496d50523f2df65955a7f559823cf18240be81
Hung Pham <pham.quang.thanh.hung@gmail.com>
2023-07-04 16:22:04 +0300

    rds database

--- level2/variables.tf
+++ level2/variables.tf
@@ -1,17 +1,17 @@
 variable "instance_type" {
-    description = "EC2 instance type"
-    type = string
-    default = "t2.micro"
+  description = "EC2 instance type"
+  type        = string
+  default     = "t2.micro"
 }
 
 variable "area_code" {
-    description = "Area code"
-    type = string
-    default = "name"
+  description = "Area code"
+  type        = string
+  default     = "name"
 }
 
 variable "key_name" {
-    description = "SSH key"
-    type = string
-    default = "main11"
+  description = "SSH key"
+  type        = string
+  default     = "main11"
 }
---
+++ modules/rds/variables.tf
@@ -0,0 +1,9 @@
+variable "area_code" {}
+
+variable "subnet_ids" {}
+
+variable "vpc_id" {}
+
+variable "source_security_group" {}
+
+variable "rds_password" {}
